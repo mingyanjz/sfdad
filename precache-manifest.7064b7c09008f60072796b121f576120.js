d5701ac812af223bb8d2336fdfa043dddc8de407
unknown <andylizd@outlook.com>
2020-10-08 17:58:08 -0400

    Updates

---
+++ precache-manifest.7064b7c09008f60072796b121f576120.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f71aaf4eaa38710e65db9272248592c",
+    "url": "/SFDispatch/index.html"
+  },
+  {
+    "revision": "e1cf5975168800c4d659",
+    "url": "/SFDispatch/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "a71894a0cfaf478e2796",
+    "url": "/SFDispatch/static/css/main.6b8ac88c.chunk.css"
+  },
+  {
+    "revision": "e1cf5975168800c4d659",
+    "url": "/SFDispatch/static/js/2.4fc6e35d.chunk.js"
+  },
+  {
+    "revision": "0624ddd414b2723e1128995f38c19298",
+    "url": "/SFDispatch/static/js/2.4fc6e35d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a71894a0cfaf478e2796",
+    "url": "/SFDispatch/static/js/main.faeafa49.chunk.js"
+  },
+  {
+    "revision": "44fe635f5ef1c2fe5eaa",
+    "url": "/SFDispatch/static/js/runtime-main.6d189d0c.js"
+  },
+  {
+    "revision": "885e97ef423883413200949f73a96134",
+    "url": "/SFDispatch/static/media/car2.885e97ef.png"
+  },
+  {
+    "revision": "9b51e7dcbe71f0285927a3441f8ff662",
+    "url": "/SFDispatch/static/media/drone.9b51e7dc.png"
+  },
+  {
+    "revision": "09a3bd601cc47d81d3199494c04be8c7",
+    "url": "/SFDispatch/static/media/drone2.09a3bd60.png"
+  },
+  {
+    "revision": "7f39bda86395a99d3114e00b1b9e9826",
+    "url": "/SFDispatch/static/media/drone_logo.7f39bda8.png"
+  },
+  {
+    "revision": "8447f75e4af9573eb61ef5175cce3821",
+    "url": "/SFDispatch/static/media/location_logo.8447f75e.png"
+  },
+  {
+    "revision": "9920085fb315816ce64e11700a15ac87",
+    "url": "/SFDispatch/static/media/redDot.9920085f.svg"
+  },
+  {
+    "revision": "6efc700d62d97e35a2445c21518e0e75",
+    "url": "/SFDispatch/static/media/robot_logo.6efc700d.png"
+  },
+  {
+    "revision": "415714caf158cfce72d561212f9939d7",
+    "url": "/SFDispatch/static/media/shark_logo.415714ca.png"
+  },
+  {
+    "revision": "0f4f45098239a033644e81f7d86ce197",
+    "url": "/SFDispatch/static/media/shipping_logo.0f4f4509.png"
+  }
+]);
